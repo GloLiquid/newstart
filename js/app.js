442d40fc48b0d990aa31657dbecefc30a779f99c
GloLiquid <glynneowen@Phillips-iMac.local>
2014-07-09 23:51:56 +0100

    test build 6

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//Problem: Toomuch info on screen at once.
+//Solution: Hide and show questions at appropriate times
+//hide hints
+$("#today").hide();
+    //Find out if password is valid  
+      //Hide hint if valid
+      //else show hint
+//When event happens on password input
+//When event happens on confirmation input
+  //Find out if password and confirmation match
+    //Hide hint if match
+    //else show hint 
+//Store information
+//Upon click, print out contents of objective
+//+time taken
+	
